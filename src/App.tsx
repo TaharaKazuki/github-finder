056adc9a04a38ebb87bd3bff71975a2be980a90c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-12 01:16:16 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,16 @@ function App() {
   return (
     <>
       <Router>
-        <div className="flex flex-col justify-between h-screen">
+        <div className="flex flex-col justify-between h-screen bg-gray-700">
           <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<Notfound />} />
-            <Route path="*" element={<Notfound />} />
-          </Routes>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<Notfound />} />
+              <Route path="*" element={<Notfound />} />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </Router>
