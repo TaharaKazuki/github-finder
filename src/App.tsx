49c65017ce572524408a2aa48a1a75aaac68d05b
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-12 15:01:26 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // layout
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
-
 // page
 import Home from './components/pages/Home'
 import About from './components/pages/About'
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -6,7 +6,13 @@ const About = () => {
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
         A React app to search Github profiles and see profile details. This
-        project is part of the
+        project is part of the React Front to Back.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Layout By: <span className="text-white">DK</span>
       </p>
     </>
   )
